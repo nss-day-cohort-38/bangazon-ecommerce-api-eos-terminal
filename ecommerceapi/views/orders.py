03089535432fbdf001b4797bea9faf1c07ca2c57
castle <castlesmadeofcode@gmail.com>
2020-05-27 09:33:17 -0500

    set up inital methods in orders.py

---
+++ ecommerceapi/views/orders.py
@@ -0,0 +1,94 @@
+"""View module for handling requests about orders"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from ecommerceapi.models import Order, Customer
+
+
+class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for orders
+    Arguments:
+        serializers
+    """
+
+    class Meta:
+        model = Order
+        url = serializers.HyperlinkedIdentityField(
+            view_name='order',
+            lookup_field='id'
+        )
+        fields = ('id', 'payment_type', 'created_at',)
+        depth = 2
+
+class OrderItems(ViewSet):
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for a single order item
+        Returns:
+            Response -- JSON serialized order instance
+        """
+        try:
+            order_item = Order.objects.get(pk=pk)
+            serializer = OrderItemSerializer(
+                order_item, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        """Handle GET requests to order resource
+        Returns:
+            Response -- JSON serialized list of customer orders
+        """
+        customer = Customer.objects.get(user=request.auth.user)
+        orders = Order.objects.filter(customer=customer)
+        serializer = OrderItemSerializer(
+            orders, many=True, context={'request': request})
+        return Response(serializer.data)
+
+        # orders = Order.objects.all()
+
+    def create(self, request):
+
+        customer = Customer.objects.get(user=request.auth.user)
+
+        new_order_item = Order()
+        new_order_item.starttime = request.data["starttime"]
+        new_order_item.customer = customer
+
+        new_order_item.save()
+
+        serializer = OrderItemSerializer(
+            new_order_item, context={'request': request})
+
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for an individual order item
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        order = Order.objects.get(pk=pk)
+        order.starttime = request.data["starttime"]
+        order.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single order item
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            order = Order.objects.get(pk=pk)
+            order.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Order.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
