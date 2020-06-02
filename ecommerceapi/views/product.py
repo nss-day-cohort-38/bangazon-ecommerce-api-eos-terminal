08489b95f54c1ce5c78e3deb88eb8b41b349b411
CoopNich <CoopNich92@gmail.com>
2020-06-02 16:14:17 -0500

    help

--- ecommerceapi/views/product.py
+++ ecommerceapi/views/product.py
@@ -20,7 +20,7 @@ class ProductSerializer(serializers.HyperlinkedModelSerializer):
             view_name='product',
             lookup_field='id'
         )
-        fields = fields = ('id', 'title', 'customer', 'price', 'description', 'quantity', 'location', 'image', 'created_at', 'product_type', 'product_type_id')
+        fields = ('id', 'title', 'customer', 'price', 'description', 'quantity', 'location', 'image', 'created_at', 'product_type', 'product_type_id')
 
 
 class Product(ViewSet):
